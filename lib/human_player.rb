6d6a639b713b87538d23a0a960ed35a7cf627b4b
7minutech <elijohnson1107@gmail.com>
2024-07-04 13:52:28 -0500

    Completed HumanPlayer class
    Class contains a guess and methods to get and
    check guess

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,7 +1,38 @@
 # defines the human player for
 # the master mind game
 class HumanPlayer
+  VALID_INPUTS = %w[r o y g b p].freeze
+
   def initialize
-    
+    @player_guess
+  end
+
+  private
+
+  attr_writer :player_guess
+
+  public
+
+  attr_reader :player_guess
+
+  def ask_player_guess
+    puts "Chose 4 colors from below like (r,o,y,g)"
+    puts "r=red, o=orange, y=yellow, g=green, b=blue, p=purple"
+    print "Enter a guess: "
+    self.player_guess = gets.chomp.downcase.split(",")
+  end
+
+  def valid_input?
+    player_guess.each do |color|
+      unless VALID_INPUTS.include?(color)
+        puts "Please enter a valid input"
+        false
+      end
+    end
+  end
+
+  def valid_guess
+    ask_player_guess
+    ask_player_guess until valid_input?
   end
 end
