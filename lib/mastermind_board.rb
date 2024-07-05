6e7a475e5e3f134c5546ceb468c326de142f12d9
7minutech <elijohnson1107@gmail.com>
2024-07-05 13:30:00 -0500

    Completed Mastermind Board class
    The class can display the board for the player
    and has methods to place player and moves
    and peg the rows on the board

--- lib/mastermind_board.rb
+++ lib/mastermind_board.rb
@@ -1,6 +1,31 @@
 # defines the master mind board object
 class MastermindBoard
+  WIDTH_OF_BOARD = (0..11).freeze
+
   def initialize
-    
+    @board = Array.new(12) { Array.new(4) { "O" } }
+    @board_divider = Array.new(12) { Array.new(1) { "|" } }
+    @peg_board = Array.new(12) { Array.new(4) { " " } }
+  end
+
+  private
+
+  attr_accessor :board, :board_divider, :peg_board, :shown_board
+
+  public
+
+  def display_board
+    WIDTH_OF_BOARD.each do |i|
+      puts (board[i] + board_divider[i] + peg_board[i]).join(" ")
+    end
+  end
+
+  def fill_player_row(row,color_arr)
+    board[row] = color_arr
+  end
+
+  def fill_peg_row(row, peg_arr)
+    peg_board[row] = peg_arr
   end
 end
+
