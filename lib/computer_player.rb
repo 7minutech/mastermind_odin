461324118ed3ef4017881e704347357681f6beb5
7minutech <elijohnson1107@gmail.com>
2024-07-05 00:40:50 -0500

    Completed computer player class
    The class contains a guess which is the code to
    break in the game and this class sets that code

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,7 +1,22 @@
 # defines a computer player for
 # the master mind game
 class ComputerPlayer
+  VALID_INPUTS = %w[r o y g b p].freeze
+
   def initialize
-    
+    @computer_guess
+  end
+
+  private
+
+  attr_accessor :computer_guess
+
+  public
+
+  def set_computer_guess
+    self.computer_guess = VALID_INPUTS.sample(4)
   end
 end
+npc = ComputerPlayer.new
+npc.set_computer_guess
+npc.display
