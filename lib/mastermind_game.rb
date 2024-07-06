968bd44dd8c1188714bf8c5f2365727c8ed5ca09
7minutech <elijohnson1107@gmail.com>
2024-07-06 11:53:46 -0500

    Updated class
    Instance variables now reference objects and
    added empty play round and game methods

--- lib/mastermind_game.rb
+++ lib/mastermind_game.rb
@@ -1,14 +1,26 @@
+require_relative "human_player"
+require_relative "computer_player"
+require_relative "mastermind_board"
 # defines the master mind game 
 # includes a human and computer player 
 # and a board
-class MasterMindGame
-  def initialize (human_player,computer_player,board)
-    @human_player = human_player
-    @computer_player = computer_player
-    @board = board
+class MastermindGame
+  def initialize
+    @human_player = HumanPlayer.new
+    @computer_player = ComputerPlayer.new
+    @board = MastermindBoard.new
   end
 
   private
-  attr_accessor :human_player, :computer_player, :board 
+
+  attr_accessor :human_player, :computer_player, :board
+
+  public
+
+  def play_round; end
+
+  def play_game; end
 end
+
+game1 = MastermindGame.new
 
