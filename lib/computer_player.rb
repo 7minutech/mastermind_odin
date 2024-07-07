8496ad0a5ad7117050dab76883629d0b02fb6d19
7minutech <elijohnson1107@gmail.com>
2024-07-07 15:09:33 -0500

    Added pool of solutions

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -2,9 +2,11 @@
 # the master mind game
 class ComputerPlayer
   VALID_INPUTS = %w[r o y g b p].freeze
+  MAX_TURNS = (0..11).freeze
 
   def initialize
     @computer_guess
+    @pool_of_solutions = (1111..6666).to_a
   end
 
   private
@@ -14,12 +16,17 @@ class ComputerPlayer
   public
 
   attr_reader :computer_guess
+  attr_accessor :pool_of_solutions
 
   def set_computer_code
     self.computer_guess = VALID_INPUTS.sample(4)
   end
 
-  def computer_guess(prev_peg_row)
+  def set_computer_guess(prev_peg_row)
+    if prev_peg_row.empty?
+      pool_of_solutions.sample(1)
+    end
+
   end
 
 
