feba35514ccab394819e9e8204ec57a929bbb512
7minutech <elijohnson1107@gmail.com>
2024-07-07 14:31:37 -0500

    Passed peg row into computer guess in guess
    Computer needs peg row to decide guess

--- lib/mastermind_game.rb
+++ lib/mastermind_game.rb
@@ -46,7 +46,7 @@ class MastermindGame
     if player.is_a?(HumanPlayer)
       player.valid_guess
     else
-      player.computer_guess
+      player.computer_guess(peg_row)
     end
   end
 
@@ -95,6 +95,7 @@ class MastermindGame
         puts "You lost!"
       else
         puts "You win!"
+      end
     end
   end
 
