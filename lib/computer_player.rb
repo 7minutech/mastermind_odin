41fe98e85ddaccd1675507df3d27c81c402e420b
7minutech <elijohnson1107@gmail.com>
2024-07-07 15:41:48 -0500

    Added methods
    Set score determines the score based on pegs and
    update gets the pegs

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -7,6 +7,8 @@ class ComputerPlayer
   def initialize
     @computer_guess
     @pool_of_solutions = (1111..6666).to_a
+    @prev_peg_row = []
+    @score = ""
   end
 
   private
@@ -16,13 +18,21 @@ class ComputerPlayer
   public
 
   attr_reader :computer_guess
-  attr_accessor :pool_of_solutions
+  attr_accessor :pool_of_solutions, :prev_peg_row, :score
 
   def set_computer_code
     self.computer_guess = VALID_INPUTS.sample(4)
   end
 
-  def set_computer_guess(prev_peg_row)
+  def set_score
+    "+#{prev_peg_row.count('b')}-#{prev_peg_row.count('w')}"
+  end
+  def update_computer_guess(peg_row)
+    self.prev_peg_row = peg_row
+    self.score = set_score
+  end
+
+  def set_computer_guess
     if prev_peg_row.empty?
       pool_of_solutions.sample(1)
     end
